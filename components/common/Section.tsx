9b6097a5db8814778ff8167d712b0eb7a9feac9b
ferys2195 <fery21irawan.me@gmail.com>
2026-03-31 21:48:49 +0700

    refactor : change div to section and passing all attribute

--- components/common/Section.tsx
+++ components/common/Section.tsx
@@ -4,15 +4,23 @@ import { LucideIcon } from "lucide-react"
 import React from "react"
 import { Separator } from "../ui/separator"
 
-interface Props {
+interface Props extends React.HTMLAttributes<HTMLElement> {
   title: string
   description?: string
   icon?: LucideIcon
   children?: React.ReactNode
 }
-export default function Section({ title, icon: Icon, children }: Props) {
+export default function Section({
+  title,
+  icon: Icon,
+  children,
+  ...rest
+}: Props) {
   return (
-    <div className="space-y-2 rounded-xl bg-card p-10 shadow">
+    <section
+      className="scroll-mt-24 space-y-2 rounded-xl bg-card p-10 shadow"
+      {...rest}
+    >
       <div className="flex items-center gap-2.5">
         {Icon && <Icon className="inline-block size-6 text-primary" />}
         <div>
@@ -21,6 +29,6 @@ export default function Section({ title, icon: Icon, children }: Props) {
       </div>
       <Separator />
       <div>{children}</div>
-    </div>
+    </section>
   )
 }
